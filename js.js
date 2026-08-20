6a360dd1374961915fffefb4dc5e5669e08ed79f
toraraffi-maker <toraraffi@gmail.com>
2026-08-20 14:34:23 +0700

    k

--- js.js
+++ js.js
@@ -192,20 +192,4 @@
                 }
             }
         }
-    import { animate, splitText, stagger } from 'animejs';
-
-const { words } = splitText('p', {
-  words: { wrap: 'clip' },
-})
-
-animate(words, {
-  y: [
-    { to: ['100%', '0%'] },
-    { to: '-100%', delay: 750, ease: 'in(3)' }
-  ],
-  duration: 750,
-  ease: 'out(3)',
-  delay: stagger(100),
-  loop: true,
-});
-        
+   
